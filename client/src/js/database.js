9573fcb11e1d4aec61f2022d0d64947cdb056ed3
ticonetster <19826920+ticonetster@users.noreply.github.com>
2022-08-18 23:30:19 -0700

    Todos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  console.log('PUT to DB');
+  //connect to DB and version we want to use
+  const jadeDb = await openDB('jate',1);
+  //make new transaction , specify the db we posting to and the data privilege of "readwrite"
+  const tx = jadeDb.transaction('jate','readwrite');
+  //open object store
+  const store = tx.objectStore('jate');
+  //use .put() method to pass in content
+  const request = store.put({ text: content });
+  //confirm teh data was added
+  const result = await request; 
+  console.log('content added to DB', result);
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET * FROM DB');
 
+  //connect to DB and version we want to use
+  const jadeDb = await openDB('jate', 1);
+  // make new transaction , specify the db we posting to and the data privilege of "readonly"
+  const tx = jadeDb.transaction('jate', 'readonly');
+  //open the object store
+  const store = tx.objectStore('jate');
+  //use .getAll() method to grab all the content in the DB
+  const request = store.getAll();
+  //confirm the data was fetched
+  const result = await request;
+  console.log('result.value', result);
+}
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,54 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Ticonetster Text Editor'
+      }),
+      //Serice Worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      //manifest.json
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Ticonetster Text Editor',
+        short_name: 'T.T.E',
+        description: 'Takes notes with Javascript syntax highlighting',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        //add babel
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
